422d3ee92315956d48551956bbee928e044b060a
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2023-04-25 08:29:01 +0200

    home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,19 @@
 import React from "react";
-import styles from './home.module.css'
+import styles from "./home.module.css";
 
 function HomeContainer() {
   return (
     <div className={styles.homeContainer}>
-      <div>Elvin Shahsuvarli</div>
+      <h1>Frontend Developer</h1>
+      <h5>
+        Transforming dreams into digital reality with the power of code and
+        creativity.
+      </h5>
+      <p className={styles.description}>
+        Experienced Frontend Developer with skills in JavaScript, HTML, CSS,
+        ReactJS, and Next.js. Creating user-friendly websites and applications
+        is my passion.
+      </p>
     </div>
   );
 }
