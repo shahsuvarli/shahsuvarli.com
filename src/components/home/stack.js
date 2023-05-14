46938b5799bfa936b607310a88b7e23b8d15f492
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2023-05-14 12:39:11 +0200

    animation added

--- src/components/home/stack.js
+++ src/components/home/stack.js
@@ -6,15 +6,22 @@ import Image from "next/image";
 function Stack() {
   return (
     <div className={styles.stack}>
-      {stack.map((item) => {
+      {stack.map((item, index) => {
         return (
-          <a href={item.href} key={item.alt} target="_blank" rel="noreferrer">
+          <a
+            href={item.href}
+            key={item.alt}
+            target="_blank"
+            rel="noreferrer"
+            className={styles.stackElement}
+          >
             <Image
               width={50}
               height={50}
               id={item.alt === "nextjs" ? styles.nextjs : undefined}
               src={item.src}
               alt={item.alt}
+              title={item.alt}
             />
           </a>
         );
